82b41ff3e7cb65d28306ade03ebcf17bdc113066
Reid Stidolph <rstidolph@128technology.com>
2021-04-22 11:07:09 -0700

    inital commit

---
+++ traffgen.js
@@ -0,0 +1,65 @@
+'use strict'
+
+// global vars
+
+const browser = '/opt/google/chrome/chrome'
+const browserArgs = ['--headless', '--disable-gpu']
+const minPageLoadInterval = 1000 // 1s
+const maxPageLoadInterval = 20000 // 20s
+const pageLoadTimeout = 10000 // 10s
+
+// import native modules
+const { spawn } = require('child_process')
+
+// import URL list
+const websites = require('./urls.json')
+
+// function for returning random number
+function getRandomNum(min, max) {
+  return Math.floor(Math.random() * ((max) - min) + min)
+}
+
+// function for spawning browser processes
+function launchBrowser(website) {
+
+  const processArgs = [ ...browserArgs, ...[website]]
+  console.log(`loading '${website}'...`)
+  return spawn(browser, processArgs)
+
+}
+
+function begin () {
+
+  let delay = getRandomNum(minPageLoadInterval, maxPageLoadInterval)
+  let website = websites[getRandomNum(0, websites.length)]
+  let processRunning = false
+
+  console.log(`waiting ${delay} ms...`)
+
+  setTimeout(()=>{
+
+    let pageLoad = launchBrowser(website)
+    processRunning = true
+
+    pageLoad.on('exit', code => {
+      console.log(`page load '${website}' process exited, return code '${code}'.`)
+      processRunning = false
+      begin()
+    })
+
+    // kill page if it has not closed after timeout
+
+    setTimeout(() => {
+      if (processRunning == true) {
+        console.log(`killing '${website}'page load process.`)
+        pageLoad.kill('SIGKILL')
+      }
+
+    }, pageLoadTimeout)
+
+  }, delay)
+
+}
+
+console.log('starting web traffic generation...')
+begin()
